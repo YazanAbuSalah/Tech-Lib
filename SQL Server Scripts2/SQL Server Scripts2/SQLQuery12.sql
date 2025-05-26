b8e37996257b2b45ba0b83152067502d4c8b210e
yazan abu salah <yazanhasanabusalah@gmail.com>
2025-05-26 22:16:23 +0300

    Active Borrowers with CTEs

--- SQL Server Scripts2/SQL Server Scripts2/SQLQuery12.sql
+++ SQL Server Scripts2/SQL Server Scripts2/SQLQuery12.sql
@@ -5,3 +5,26 @@ JOIN Borrowers bo ON l.BorrowerID = bo.BorrowerID
 WHERE bo.FirstName = 'FirstName1' and bo.LastName = 'LastName1';
 
 
+--2
+
+WITH ActiveBorrowers AS (
+    SELECT 
+        b.BorrowerID,
+        b.FirstName,
+        b.LastName
+    FROM Borrowers b
+    JOIN Loans l ON b.BorrowerID = l.BorrowerID
+	WHERE DateReturned IS NULL
+      AND b.BorrowerID NOT IN (
+          SELECT BorrowerID 
+          FROM Loans 
+          WHERE DateReturned IS NOT NULL
+      )
+    GROUP BY b.BorrowerID, FirstName, LastName
+    HAVING COUNT(*) >= 2
+)
+SELECT FirstName, LastName
+FROM ActiveBorrowers;
+
+
+
